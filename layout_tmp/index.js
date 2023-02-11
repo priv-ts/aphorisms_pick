417d43a08d8acd46e637fc02e86f2e020c52c0ce
privts <priv.tomokisato@gmail.com>
2023-02-11 13:59:30 +0900

    viteのインストール
    - npm updateをしない状態だとエラー（sh: vite: command not found）になるので注意
    - 一旦既存の内容は別ディレクトリに退避した
    - 既存の内容を戻すコミットは別で作る

--- layout_tmp/index.js
+++
@@ -1,8 +0,0 @@
-const headerNav = document.querySelector(".header__navigation");
-const headerMenuIcon = document.querySelector(".header__sticky__menu");
-const contentMask = document.querySelector(".mask");
-
-headerMenuIcon.addEventListener("click", () => {
-  headerNav.classList.toggle("open");
-  headerMenuIcon.classList.toggle("open");
-});
